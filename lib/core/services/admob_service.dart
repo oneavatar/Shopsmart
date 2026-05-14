2f8f51c53925fbe52594e36ac39f6bf5d38da128
Sohan Thapa <your_email@example.com>
2026-05-14 22:10:34 +0545

    Updated development mode and real add for app

--- lib/core/services/admob_service.dart
+++ lib/core/services/admob_service.dart
@@ -1,10 +1,34 @@
+import 'package:flutter/foundation.dart';
+import 'dart:io';
 
 class AdMobService {
-  // TEST BANNER ID
-  static String bannerAdUnitId = 'ca-app-pub-1055768285955437/6903812121';
+  // BANNER ID
+  static String get bannerAdUnitId {
+    if (kDebugMode) {
+      return Platform.isAndroid
+          ? 'ca-app-pub-3940256099942544/6300978111'
+          : 'ca-app-pub-3940256099942544/2934735716';
+    }
+    return 'ca-app-pub-1055768285955437/6903812121';
+  }
 
-  // TEST INTERSTITIAL ID
-  static String interstitialAdUnitId = 'ca-app-pub-1055768285955437/3191192586';
-  // TEST REWARDED ID
-  static String rewardedAdUnitId = 'ca-app-pub-1055768285955437/4776342234';
+  // INTERSTITIAL ID
+  static String get interstitialAdUnitId {
+    if (kDebugMode) {
+      return Platform.isAndroid
+          ? 'ca-app-pub-3940256099942544/1033173712'
+          : 'ca-app-pub-3940256099942544/4411468910';
+    }
+    return 'ca-app-pub-1055768285955437/3191192586';
+  }
+
+  // REWARDED ID
+  static String get rewardedAdUnitId {
+    if (kDebugMode) {
+      return Platform.isAndroid
+          ? 'ca-app-pub-3940256099942544/5224354917'
+          : 'ca-app-pub-3940256099942544/1712485313';
+    }
+    return 'ca-app-pub-1055768285955437/4776342234';
+  }
 }
