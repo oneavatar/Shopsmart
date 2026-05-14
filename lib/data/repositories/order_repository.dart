38222bcbd920ed17e8915bd5f0784a20818d550d
Sohan Thapa <your_email@example.com>
2026-05-14 21:28:31 +0545

    Updated ShopSmart app

--- lib/data/repositories/order_repository.dart
+++ lib/data/repositories/order_repository.dart
@@ -1,15 +1,27 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:shopsmart/data/models/order_model.dart';
 
 class OrderRepository {
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
 
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
   Future<void> placeOrder(OrderModel order) async {
-    await firestore.collection('orders').doc(order.id).set(order.toJson());
+    await firestore.collection('orders').doc(order.id).set({
+      ...order.toJson(),
+
+      'userId': auth.currentUser!.uid,
+    });
   }
 
   Future<List<OrderModel>> fetchOrders() async {
-    final snapshot = await firestore.collection('orders').get();
+    final snapshot = await firestore
+        .collection('orders')
+        .where('userId', isEqualTo: auth.currentUser!.uid)
+        .orderBy('createdAt', descending: true)
+        .get();
 
     return snapshot.docs.map((doc) {
       return OrderModel.fromJsonMap(doc.data());
